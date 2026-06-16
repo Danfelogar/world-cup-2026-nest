a2d97f1545ea92ad2ecfdfca6752a6258374f403
DanfelogarDev <danypolo18@hotmail.com>
2026-06-16 09:02:33 -0500

    feat: adding main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json, urlencoded } from 'express';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.use(json({ limit: '10mb' }));
+  app.use(urlencoded({ extended: true, limit: '10mb' }));
+  app.enableCors();
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  Logger.log(`🚀 Server running on http://localhost:${port}`);
 }
 bootstrap();
