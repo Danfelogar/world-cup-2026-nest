717e7f4d9ea1b5956e39ea1e41e001139af3bd97
DanfelogarDev <danypolo18@hotmail.com>
2026-06-22 15:00:15 -0500

    feat: app.modules.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from '../prisma/prisma.module';
+import { TeamsModule } from './teams/teams.module';
+import { ScrapingModule } from './scraping/scraping.module';
+import { PredictionModule } from './prediction/prediction.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    PrismaModule,
+    TeamsModule,
+    ScrapingModule,
+    PredictionModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
